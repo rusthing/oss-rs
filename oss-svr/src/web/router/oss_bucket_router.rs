dd9c1f66605ab5fcee39c0e74fbd011a87b945c6
zbz <nnzbz@qq.com>
2026-03-10 09:59:08 +0800

    + 添加save路由

--- oss-svr/src/web/router/oss_bucket_router.rs
+++ oss-svr/src/web/router/oss_bucket_router.rs
@@ -8,6 +8,7 @@ pub(super) fn routes(router: Router) -> Router {
     router
         .route("/oss/bucket", post(oss_bucket_ctrl::add))
         .route("/oss/bucket", put(oss_bucket_ctrl::modify))
+        .route("/oss/bucket/save", post(oss_bucket_ctrl::save))
         .route("/oss/bucket/{id}", delete(oss_bucket_ctrl::del))
         .route("/oss/bucket/{id}", get(oss_bucket_ctrl::get_by_id))
         .route(
