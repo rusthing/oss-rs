89beab38fb2475574f8c39a55de319477637d32b
nnzbz <nnzbz@qq.com>
2026-08-11 22:47:37 +0800

    * 临时提交

--- oss-svr/src/app/app_config.rs
+++ oss-svr/src/app/app_config.rs
@@ -4,21 +4,6 @@ use robotech::db::DbConnConfig;
 use robotech::web::WebServerConfig;
 use serde::Deserialize;
 
-// static APP_CONFIG: RwLock<Option<AppConfig>> = RwLock::new(None);
-//
-// /// 获取App配置的只读访问
-// pub fn get_app_config() -> Result<AppConfig, AppError> {
-//     let read_lock = APP_CONFIG.read().map_err(|_| AppError::GetAppConfig())?;
-//     read_lock.clone().ok_or(AppError::GetAppConfig())
-// }
-//
-// /// 设置App配置
-// pub fn set_app_config(value: AppConfig) -> Result<(), AppError> {
-//     let mut write_lock = APP_CONFIG.write().map_err(|_| AppError::SetAppConfig())?;
-//     *write_lock = Some(value);
-//     Ok(())
-// }
-
 /// 配置文件结构
 #[derive(Debug, Deserialize, Clone)]
 #[serde(rename_all = "kebab-case")]
