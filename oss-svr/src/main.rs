ae2502b980955a85bb1c72e572dc4a9133502d38
nnzbz <nnzbz@qq.com>
2026-09-01 17:04:33 +0800

    * 临时提交

--- oss-svr/src/main.rs
+++ oss-svr/src/main.rs
@@ -10,6 +10,7 @@ use robotech::db::setup_db_conn;
 use robotech::env::init_env;
 use robotech::log::LogWatcher;
 use robotech::macros::{db_migrate, log_call};
+use robotech::micro_svc::{deregister, register};
 use robotech::signal::SignalManager;
 use robotech::web::{setup_web_server, stop_web_service};
 use std::collections::HashMap;
@@ -89,6 +90,9 @@ async fn main() -> anyhow::Result<()> {
             let changed = Some(changed);
             setup(&app_config, &changed, port, old_pid).await?;
 
+            // 端口可能变了，重新注册（内部判断端口是否真的变了，变了会先注销再注册）
+            register().await?;
+
             info!("重新加载配置成功");
             Ok(())
         },
@@ -98,9 +102,13 @@ async fn main() -> anyhow::Result<()> {
     let changed = None;
     setup(&app_watcher.app_config, &changed, port, old_pid).await?;
 
+    // 注册服务到注册中心
+    register().await?;
+
     // 监听系统信号与等待退出
     let signal_receiver = signal_manager.watch_signal()?;
     Ok(wait_app_exit(signal_receiver, || async move {
+        deregister().await?;
         stop_web_service().await.expect("无法停止旧的Web服务");
         Ok(())
     })
